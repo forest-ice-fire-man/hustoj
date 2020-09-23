7b7cc7946576a027f536a8bc78faed557ab606ee
forest-ice-fire-man <61698142+forest-ice-fire-man@users.noreply.github.com>
2020-09-23 14:00:45 +0800

    Update okcalls64.h

--- trunk/core/judge_client/okcalls64.h
+++ trunk/core/judge_client/okcalls64.h
@@ -21,7 +21,7 @@ int LANG_CV[CALL_ARRAY_SIZE] = {
         SYS_read, SYS_write, SYS_mprotect, SYS_munmap, SYS_brk, SYS_arch_prctl, SYS_pread64, SYS_open, SYS_writev,
         SYS_time, SYS_futex, SYS_set_thread_area, SYS_access, SYS_clock_gettime, SYS_exit_group, SYS_mq_open,
         SYS_ioprio_get, SYS_unshare, SYS_set_robust_list, SYS_splice, SYS_close, SYS_stat, SYS_fstat, SYS_execve,
-        SYS_uname, SYS_lseek, SYS_readlink, SYS_mmap, SYS_sysinfo, 0 };
+        SYS_uname, SYS_lseek, SYS_readlink, SYS_mmap, SYS_sysinfo , 25 , 0 };
 //pascal
 int LANG_PV[CALL_ARRAY_SIZE] = {
         SYS_read, SYS_write, SYS_mprotect, SYS_munmap, SYS_brk, SYS_rt_sigaction, SYS_arch_prctl, SYS_ioctl,
